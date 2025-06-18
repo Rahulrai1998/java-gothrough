774988af3b928fc47658b5eb41e189152dd5378f
Rahul Kumar <rahulrai2822march@gmail.com>
2025-06-18 07:43:59 +0530

    oops started

---
+++ src/oops/Car.java
@@ -0,0 +1,14 @@
+package oops;
+
+public class Car {
+    String color;
+    String brand;
+    String model;
+    int year;
+    int spead;
+
+    public void accelerate(int increment){
+        spead+=increment;
+    }
+
+}
---
+++ src/oops/Main.java
@@ -0,0 +1,11 @@
+package oops;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Car car = new Car();
+        car.color = "Red";
+
+        System.out.println(car.color);
+    }
+}
